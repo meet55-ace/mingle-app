3559b05c1787b5e26d78ec58e839251ad70282c5
meet <meetpadaliya55@gmail.com>
2025-05-14 10:25:31 +0530

    update package and authStore

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASEURL =
+const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
@@ -89,7 +89,7 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
 
     if (!authUser || get().socket?.connected) return;
-    const socket = io(BASEURL, {
+    const socket = io(BASE_URL, {
       query: {
         userId: authUser._id,
       },
