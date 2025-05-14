742aaec5006b932c50e1f7e788ed39315a89ca57
meet <meetpadaliya55@gmail.com>
2025-05-14 17:42:43 +0530

    change in authstore

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -48,7 +48,7 @@ export const useAuthStore = create((set, get) => ({
   login: async (data) => {
     set({ isLoggining: true });
     try {
-      const res = await axiosInstance.post("auth/login", data);
+      const res = await axiosInstance.post("/auth/login", data);
       set({ authUser: res.data });
       toast.success("Logged in successfully");
 
@@ -62,7 +62,7 @@ export const useAuthStore = create((set, get) => ({
 
   logout: async () => {
     try {
-      await axiosInstance.post("auth/logout");
+      await axiosInstance.post("/auth/logout");
       set({ authUser: null });
       toast.success("Logged out successfully");
       get().disconnectSocket();
